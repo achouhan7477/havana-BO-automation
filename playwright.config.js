fdd7f32bd7f7c3afa3ed7df340bea3606c85c5d3
Aman Chouhan <achouhan7477@users.noreply.github.com>
2026-01-08 18:45:08 +0530

    git ignore

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
+  testDir: "./tests",
+
+  // 🔴 Ignore Jest-style / API tests
+  testIgnore: [
+    "**/tests/auth/**",
+    "**/tests/bonus/**",
+    "**/tests/games/**",
+    "**/tests/users/**",
+    "**/tests/wallet/**",
+  ],
+
   // globalSetup: "./global-setup.js",
 
   use: {
@@ -8,7 +19,7 @@ export default defineConfig({
     storageState: "auth.json",
     browserName: "chromium",
     headless: false,
-    slowMo: 500,               // 🔥 UI slow dikhe
+    slowMo: 500,
     viewport: { width: 1440, height: 900 },
     actionTimeout: 20 * 1000,
     navigationTimeout: 45 * 1000,
